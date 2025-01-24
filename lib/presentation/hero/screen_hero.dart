9d8d7c9ab3145feb458b2aed786a358dacecec3b
shaaanuu <monshan50@gmail.com>
2025-01-24 18:00:39 +0530

    remove: screentrending

--- lib/presentation/hero/screen_hero.dart
+++ lib/presentation/hero/screen_hero.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../categories/screen_categories.dart';
 import '../home/screen_home.dart';
-import '../trending/screen_trending.dart';
 
 class ScreenHero extends StatelessWidget {
   final ValueNotifier<int> btmnavbarNotifier = ValueNotifier(0);
@@ -13,7 +12,6 @@ class ScreenHero extends StatelessWidget {
     List<Widget> pages = [
       const ScreenHome(),
       const ScreenCategories(),
-      const ScreenTrending(),
     ];
 
     List<NavigationDestination> navigationDestinations = const [
@@ -27,35 +25,11 @@ class ScreenHero extends StatelessWidget {
         label: 'Categories',
         selectedIcon: Icon(Icons.category),
       ),
-      NavigationDestination(
-        icon: Icon(Icons.trending_up_outlined),
-        label: 'Trending',
-        selectedIcon: Icon(Icons.trending_up),
-      ),
     ];
 
     return ValueListenableBuilder(
       valueListenable: btmnavbarNotifier,
       builder: (context, int currentIndex, _) => Scaffold(
-        // appBar: AppBar(
-        //   title: const Text(
-        //     "MovieMania",
-        //     style: TextStyle(letterSpacing: 1, fontWeight: FontWeight.w400),
-        //   ),
-        //   actions: [
-        //     Padding(
-        //       padding: const EdgeInsets.only(right: 10),
-        //       child: IconButton.filled(
-        //         style: IconButton.styleFrom(
-        //           backgroundColor: const Color(0xFF45475a),
-        //           foregroundColor: const Color(0xFFcdd6f4),
-        //         ),
-        //         onPressed: () {},
-        //         icon: const Icon(Icons.search),
-        //       ),
-        //     ),
-        //   ],
-        // ),
         body: pages[currentIndex],
         bottomNavigationBar: NavigationBar(
           destinations: navigationDestinations,
